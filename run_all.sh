7765fb24b8bf3353befd4562691797ccac1eda27
Matan Gizunterman <Matan.Gizunterman@arm.com>
2016-09-13 21:28:43 +0300

    lasd

--- run_all.sh
+++ run_all.sh
@@ -1,25 +1,7 @@
 set -ex
 git pull
-make -f SPLoggerTest.make clean 
-make -f SPLoggerTest.make 
+make clean 
+make 
 echo
 echo
-valgrind ./sp_logger_unit_test 
-echo
-echo
-make -f SPBPriorityQueueTest.make clean 
-make -f SPBPriorityQueueTest.make 
-echo
-echo
-valgrind ./sp_bpqueue_unit_test 
-echo
-echo
-make -f SPPointTest.make clean 
-make -f SPPointTest.make
-echo
-echo
-valgrind ./sp_point_unit_test
- 
-
-
-
+./SPCBIR -c myconfig.config
